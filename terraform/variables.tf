f0f1d5bfc3750785d8ad5d0bd763ab43c9970597
Enes Demir <123deli4567@gmail.com>
2026-04-24 00:01:11 +0300

    fix(infra): downgrade EKS to 1.28 for Frankfurt AMI compatibility

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,7 +19,7 @@ variable "environment" {
 variable "eks_cluster_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "1.29"
+  default     = "1.28"
 }
 
 variable "node_instance_type" {
